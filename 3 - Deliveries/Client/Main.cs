d902546d2d2c7bcc143bcd4f451977a5462e64d3
Michael <mkmike@gmail.com>
2020-12-09 10:17:02 -0600

    Add Episode 3

---
+++ 3 - Deliveries/Client/Main.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+namespace Client
+{
+    public class Main : BaseScript
+    {
+        private int startBlipId;
+        private int destinationBlipId;
+        private int vehicleId;
+        private int vehicleBlipId;
+        private List<Vector3> jobDestinations = new List<Vector3> { };
+
+        private bool canStartMission = false;
+        private bool waitingToStart = false;
+        private bool inProgress = false;
+        private bool isEnding = false;
+        private bool waitingForCarCleanup = false;
+
+        private List<string> cartModels = new List<string> { "CART05", "CART07" };
+        private Vector3 startLocation = new Vector3(-311F, 806F, 118F);
+        private Vector3 vehStartLocation = new Vector3(-316F, 824F, 118F);
+        private List<Vector3> baseDestinations = new List<Vector3> {
+            new Vector3(-278.58F, 644.33F, 113.84F),
+            new Vector3(-251.21F, 770.13F, 117.48F),
+            new Vector3(-280.01F, 868.88F, 120.87F)
+        };
+
+        private readonly Random random = new Random();
+
+        public Main()
+        {
+            API.RegisterCommand("worldpos", new Action(Util.WorldPositionFetcher), false);
+
+            Util.worldMarkers.Add(startLocation);
+            startBlipId = Util.AddBlipForCoords(2033377404, startLocation);
+            API.SetBlipNameFromTextFile(startBlipId, "BLIP_SALOON");
+            canStartMission = true;
+        }
+
+        [Tick]
+        private async Task OnTick()
+        {
+            int playerPedId = API.PlayerPedId();
+            Vector3 playerPos = API.GetEntityCoords(playerPedId, true, true);
+
+            // Check for starting the mission
+            if (canStartMission)
+            {
+                if(API.GetDistanceBetweenCoords(playerPos.X, playerPos.Y, playerPos.Z, startLocation.X, startLocation.Y, startLocation.Z, true) <= 2F)
+                {
+                    Util.DisplayHelpText("Press T to start a delivery mission");
+
+                    // Accept input
+                    if(API.IsControlJustPressed(API.PlayerPedId(), 0x9720FCEE))
+                    {
+                        StartDelivery();
+                    }
+                }
+            }
+
+            // Wait for Player to get in vehicle
+            if (waitingToStart)
+            {
+                if(API.GetPlayersLastVehicle() != 0 && API.GetPlayersLastVehicle() == vehicleId)
+                {
+                    waitingToStart = false;
+                    inProgress = true;
+                    API.RemoveBlip(ref vehicleBlipId);
+                    vehicleBlipId = Util.AddBlipForEntity(vehicleId, -1749618580, false);
+                    destinationBlipId = Util.AddBlipForCoords(-308585968, jobDestinations[0], true);
+                    Util.DisplayText("Proceed to the first delivery destination.");
+                }
+            }
+
+            // Check progress
+            if (inProgress)
+            {
+                VehicleChecks();
+
+                // Check if at destination
+                Vector3 vehiclePos = API.GetEntityCoords(vehicleId, true, true);
+                if(API.GetDistanceBetweenCoords(vehiclePos.X, vehiclePos.Y, vehiclePos.Z, jobDestinations[0].X, jobDestinations[0].Y, jobDestinations[0].Z, true) <= 5F){
+                    if(API.GetEntityVelocity(playerPedId, 0) == Vector3.Zero)
+                    {
+                        Util.DisplayHelpText("Press T to deliver");
+                        if(API.IsControlJustPressed(API.PlayerPedId(), 0x9720FCEE))
+                        {
+                            API.RemoveBlip(ref destinationBlipId);
+                            jobDestinations.RemoveAt(0);
+                            if(jobDestinations.Count != 0)
+                            {
+                                destinationBlipId = Util.AddBlipForCoords(-308585968, jobDestinations[0], true);
+                                Util.DisplayText("Proceed to the next delivery destination.");
+                            }
+                            else
+                            {
+                                inProgress = false;
+                                isEnding = true;
+                                destinationBlipId = Util.AddBlipForCoords(-308585968, vehStartLocation, true);
+                                Util.DisplayText("Drop the wagon back off at the Saloon.");
+                            }   
+                        }
+                    }
+                    else
+                    {
+                        Util.DisplayText("Come to a complete stop to complete this delivery.", 500);
+                    }
+                }
+            }
+            
+            // Check if ending
+            if (isEnding)
+            {
+                VehicleChecks();
+
+                // Check if at destination
+                Vector3 vehiclePos = API.GetEntityCoords(vehicleId, true, true);
+                if (API.GetDistanceBetweenCoords(vehiclePos.X, vehiclePos.Y, vehiclePos.Z, vehStartLocation.X, vehStartLocation.Y, vehStartLocation.Z, true) <= 2F)
+                {
+                    if (!API.IsPedInAnyVehicle(playerPedId, true))
+                    {
+                        isEnding = false;
+                        CleanUp();
+                        Util.DisplayText("Thank you for the quick work. Come by any time for another job.");
+                        API.PlaySoundFrontend("REWARD_NEW_GUN", "HUD_REWARD_SOUNDSET", true, 0);
+                        // API.MoneyIncrementCashBalance(500, 752097756);
+                    }
+                    else
+                    {
+                        if (API.GetEntityVelocity(playerPedId, 0) == Vector3.Zero)
+                        {
+                            // At destination
+                            if (API.IsPedInAnyVehicle(playerPedId, true))
+                            {
+                                Util.DisplayHelpText("Exit the vehicle to complete the job");
+                            }
+                        }
+                        else
+                        {
+                            Util.DisplayText("Come to a complete stop to complete this job.", 500);
+                        }
+                    }
+                }
+            }
+
+            // Clean car up
+            if (waitingForCarCleanup)
+            {
+                if (!API.IsEntityOnScreen(vehicleId) || API.IsEntityOccluded(vehicleId))
+                {
+                    waitingForCarCleanup = false;
+                    API.DeleteEntity(ref vehicleId);
+                    vehicleId = -1;
+                }
+            }
+        }
+
+        private void VehicleChecks()
+        {
+            if (!API.IsPedInAnyVehicle(API.PlayerPedId(), true))
+            {
+                Util.DisplayHelpText("Get back onto the wagon");
+            }
+            if (!HealthCheck())
+            {
+                waitingToStart = false;
+                inProgress = false;
+                isEnding = false;
+                CleanUp();
+            }
+        }
+
+        private bool HealthCheck()
+        {
+            return API.IsVehicleDriveable(vehicleId, 1, 1) || !API.IsPlayerDead(API.PlayerPedId());
+        }
+
+        private void CleanUp()
+        {
+            canStartMission = true;
+            Util.worldMarkers.Add(startLocation);
+            startBlipId = Util.AddBlipForCoords(2033377404, startLocation);
+            API.SetBlipNameFromTextFile(startBlipId, "BLIP_SALOON");
+            API.RemoveBlip(ref vehicleBlipId);
+            API.RemoveBlip(ref destinationBlipId);
+            API.SetVehicleAsNoLongerNeeded(ref vehicleId);
+            waitingForCarCleanup = true;
+        }
+
+        private async void StartDelivery()
+        {
+            canStartMission = false;
+            waitingToStart = true;
+            Util.worldMarkers.Remove(startLocation);
+            jobDestinations = new List<Vector3>(baseDestinations);
+            API.RemoveBlip(ref startBlipId);
+            API.PlaySoundFrontend("REWARD_NEW_GUN", "HUD_REWARD_SOUNDSET", true, 0);
+            string cartModel = cartModels[random.Next(cartModels.Count)];
+            await Util.LoadModel(cartModel);
+            vehicleId = API.CreateVehicle((uint)API.GetHashKey(cartModel), vehStartLocation.X, vehStartLocation.Y, vehStartLocation.Z, 90F, true, true, false, true);
+            API.SetEntityAsMissionEntity(vehicleId, true, true);
+            vehicleBlipId = Util.AddBlipForEntity(vehicleId, -308585968);
+            Util.DisplayText("Thanks for taking this job. The wagon is out back.");
+        }
+    }
+}
---
+++ 3 - Deliveries/Client/Util.cs
@@ -0,0 +1,125 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Util : BaseScript
+    {
+        static int subtitleTimeEnd;
+        static string subtitleText = null;
+        public static List<Vector3> worldMarkers = new List<Vector3> { };
+
+        [Tick]
+        private static async Task OnTick()
+        {
+            if (subtitleText != null)
+            {
+                DrawText(subtitleText, 23, 0.5F, 0.85F, 0.50F, 0.40F, 255, 255, 255, 255);
+
+                if (subtitleTimeEnd <= API.GetGameTimer())
+                {
+                    subtitleText = null;
+                }
+            }
+            
+            foreach(Vector3 pos in worldMarkers)
+            {
+                Function.Call((Hash)0x2A32FAA57B937173, -1795314153, pos.X, pos.Y, pos.Z, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 0.9, 255, 255, 0, 155, 0, 0, 2, 0, 0, 0, 0);
+            }
+        }
+
+        public static void EnableBlipGPS(int blipId)
+        {
+            Function.Call((Hash)0x662D364ABF16DE2F, blipId, 0x900A4D0A);
+        }
+
+        public static int AddBlipForCoords(int sprite, Vector3 pos, bool enableGps = false)
+        {
+            int blipId = Function.Call<int>((Hash)0x554D9D53F696D002, (uint)sprite, pos.X, pos.Y, pos.Z);
+            if (enableGps)
+            {
+                EnableBlipGPS(blipId);
+            }
+            return blipId;
+        }
+
+        public static int AddBlipForEntity(int entity, int spriteId, bool enableGps = true)
+        {
+            int blipId = Function.Call<int>((Hash)0x23F74C2FDA6E7C61, spriteId, entity);
+            if (enableGps)
+            {
+                EnableBlipGPS(blipId);
+            }
+            return blipId;
+        }
+
+        public static void DisplayText(string text, int duration = 5000)
+        {
+            subtitleTimeEnd = API.GetGameTimer() + duration;
+            subtitleText = text;
+        }
+
+        public static void DisplayHelpText(string text)
+        {
+            DrawText(text, 23, 0.5F, 0.9F, 0.50F, 0.40F, 125, 255, 255, 255);
+        }
+
+        public static void DrawText(string text, int fontId, float x, float y, float scaleX, float scaleY, int r, int g, int b, int a)
+        {
+            // Draw Text
+            API.SetTextScale(scaleX, scaleY);
+            API.SetTextColor(r, g, b, a);
+            API.SetTextCentre(true);
+            Function.Call((Hash)0xADA9255D, fontId); // Loads the font requested
+            API.DisplayText(Function.Call<long>(Hash._CREATE_VAR_STRING, 10, "LITERAL_STRING", text), x, y);
+
+            // Draw Backdrop
+            float lineLength = (float) text.Length / 100 * 0.66F;
+            DrawTexture("boot_flow", "selection_box_bg_1d", x, y, lineLength, 0.035F, 0F, 0, 0, 0, 200);
+        }
+
+        public static async void DrawTexture(string textureDict, string textureName, float x, float y, float width, float height, float rotation, int r, int g, int b, int a)
+        {
+            if (!API.HasStreamedTextureDictLoaded(textureDict))
+            {
+                API.RequestStreamedTextureDict(textureDict, false);
+                while (!API.HasStreamedTextureDictLoaded(textureDict))
+                {
+                    Debug.WriteLine($"{textureDict}.{textureName} is waiting to load...");
+                    await BaseScript.Delay(100);
+                }
+            }
+            API.DrawSprite(textureDict, textureName, x, y + 0.015F, width, height, rotation, r, g, b, a, true);
+        }
+
+        public static async Task<bool> LoadModel(string model)
+        {
+            int hash = API.GetHashKey(model);
+            if (!API.IsModelValid((uint)hash))
+            {
+                Debug.WriteLine($"Model {model} is not valid and could not be loaded.");
+                return false;
+            }
+            if (!API.HasModelLoaded((uint)hash))
+            {
+                API.RequestModel((uint)hash, true);
+                while (!API.HasModelLoaded((uint)hash))
+                {
+                    Debug.WriteLine($"Model {model} is waiting to load...");
+                    await BaseScript.Delay(100);
+                }
+            }
+            return true;
+        }
+
+        public static void WorldPositionFetcher()
+        {
+            int playerPedId = API.PlayerPedId();
+            Vector3 coords = API.GetEntityCoords(playerPedId, true, true);
+            float h = API.GetEntityHeading(playerPedId);
+            Debug.WriteLine($"X: {coords.X} Y: {coords.Y} Z: {coords.Z} H: {h}");
+        }
+    }
+}
